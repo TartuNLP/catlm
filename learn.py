5666ee1e7a1ee15fa23035e5281a8da076774557
Mark Fishel <fishel@ut.ee>
2017-12-05 13:14:29 +0200

    added time logs

--- learn.py
+++ learn.py
@@ -5,6 +5,10 @@ import rnnlm
 import txt
 import pickle
 from keras.models import load_model
+from datetime import datetime
+
+def log(msg):
+	print("LOG", str(datetime.now()), msg)
 
 if __name__ == "__main__":
 	if len(sys.argv) == 4:
@@ -16,14 +20,22 @@ if __name__ == "__main__":
 		maxLen = 50
 		doChars = True
 		
+		log("load data")
 		txtdata, params = txt.loadAndClean(dataFile, maxLen, chars = doChars)
 		
+		log("save params")
 		rnnlm.saveParams(params, paramOutFile)
 		
+		log("init model")
 		lm = rnnlm.initModelNew(params, embSize = (32 if doChars else 256))
 		
+		log("learn model")
 		rnnlm.learn(lm, params, txtdata)
+		
+		log("save model")
 		lm.save(modelOutFile)
+		
+		log("done")
 		
 	elif len(sys.argv) == 5:
 		#continue learning
@@ -32,12 +44,19 @@ if __name__ == "__main__":
 		modelInFile = sys.argv[3]
 		modelOutFile = sys.argv[4]
 		
+		log("load model and params")
 		(lm, params) = rnnlm.loadModels(modelInFile, paramInFile)
 		
+		log("load data")
 		txtData, _, _ = txt.loadFile(dataFile, maxLen = params.max, chars = True)
-		data = txt.getIOData(txtData, params)
+		#data = txt.getIOData(txtData, params)
 		
-		rnnlm.learn(lm, params, data)
+		log("learn model")
+		rnnlm.learn(lm, params, txtData)
+		
+		log("save model")
 		lm.save(modelOutFile)
+		
+		log("done")
 	else:
 		raise Exception("AAAAA")
